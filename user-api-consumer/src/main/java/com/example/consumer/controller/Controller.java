00bae73a20e14b2fe31bf35326860b0fa15f7c49
deepakbhalla <deepak.dbhalla@gmail.com>
2021-02-08 12:59:09 +0530

    added another api call in controller

--- user-api-consumer/src/main/java/com/example/consumer/controller/Controller.java
+++ user-api-consumer/src/main/java/com/example/consumer/controller/Controller.java
@@ -2,6 +2,7 @@ package com.example.consumer.controller;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,16 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.consumer.model.Response;
 import com.example.consumer.model.UserData;
+import com.example.consumer.model.UserDetail;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
+
+	private static final String URL_1 = "https://jsonplaceholder.typicode.com/users";
+	private static final String URL_2 = "https://jsonplaceholder.typicode.com/comments";
 
 	@Autowired
 	RestTemplate restTemplate;
@@ -33,10 +38,14 @@ public class Controller {
 		return "I am working fine";
 	}
 
-	@GetMapping("/details")
+	/**
+	 * Reads the response from URL_1 and performs some filter operation and returns the data.
+	 * @return
+	 */
+	@GetMapping("/details/url/1")
 	public ResponseEntity<List<Response>> getDetails() {
 
-		ResponseEntity<String> response = restTemplate.exchange("https://jsonplaceholder.typicode.com/users",
+		ResponseEntity<String> response = restTemplate.exchange(URL_1,
 				HttpMethod.GET, HttpEntity.EMPTY, String.class);
 
 		// UserData[] userDataDetails = gson.fromJson(response.getBody(), UserData[].class);
@@ -52,5 +61,34 @@ public class Controller {
 		}
 
 		return ResponseEntity.ok().body(filteredResponse);
+	}
+	
+	@GetMapping("/details/url/2")
+	public ResponseEntity<UserDetail[]> getFilteredDetails() {
+
+		ResponseEntity<String> response = restTemplate.exchange(URL_2, HttpMethod.GET, HttpEntity.EMPTY, String.class);
+
+		UserDetail[] details = gson.fromJson(response.getBody(), UserDetail[].class);
+
+		/** Filter/update response, solution 1 (change response type to ResponseEntity<List<UserDetail>) */
+		/*List<UserDetail> listUsers = new ArrayList<UserDetail>();
+		for (UserDetail dt : details) {
+			if (dt.getPostId() == 1) {
+				dt.setBody("Id ".concat(String.valueOf(dt.getId())));
+				listUsers.add(dt);
+			}
+		}*/
+
+		/** Filter/update response, solution 2 (change response type to ResponseEntity<List<UserDetail>) */
+		/* List<UserDetail> listUsers = 
+		 		Arrays.asList(details).stream().filter(u -> (u.getPostId() == 1)).collect(Collectors.toList());
+		   listUsers.stream().forEach(a -> a.setBody("Id ".concat(String.valueOf(a.getId())))); 
+		*/
+		
+		/** Filter/update response, solution 3 */
+		Arrays.asList(details).stream().filter(u -> (u.getPostId() == 1))
+			.forEach(a -> a.setBody("Id ".concat(String.valueOf(a.getId()))));
+
+		return ResponseEntity.ok().body(details);
 	}
 }
